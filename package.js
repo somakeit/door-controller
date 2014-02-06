8f2966d012e120ee3e24525fdccea4ce1c9d8be4
Anton Piatek <anton@piatek.co.uk>
2014-02-06 16:14:13 +0000

    add package files

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "cardReader",
+  "version": "0.0.1",
+  "private":true,
+  "description": "SoMakeIt card reader",
+  "main": "run.js",
+  "scripts": {
+    "test": "nodeunit tests.js"
+  },
+  "author": "Anton Piatek",
+  "license": "MIT"
+}
+
