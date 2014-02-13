3f9e9541e3f579b9f89a426026aecb45357c21ce
Anton Piatek <anton@piatek.co.uk>
2014-02-13 08:12:47 +0000

    start of http refresh

--- cardReader.js
+++ cardReader.js
@@ -1,23 +1,28 @@
 var fs = require('fs');
+var http = require('http');
+var https = require('https');
 //winston.handleExceptions(); //TODO: need to give explicit handler
 
 function CardReader(params) {
   this.winston = params.winston;
   this.device = params.device; //TODO throw if unset
   this.cardsFile = params.knownCardsFile;
-  this.winston.log('info',"Loading reader for device/file '%s' with known cards file '%s'", this.device, this.knownCardsFile);
+  this.winston.log('info',"Loading reader for device/file '%s'", this.device);
   this.knownCards = {};
   this.readStream = fs.createReadStream(this.device); //TODO throw if unset
 
-  this._loadKnownCards();
   //Watch for new file
   var self = this;
-  fs.watchFile(this.knownCardsFile, function(curr,prev) {
-    if (curr.mtime != prev.mtime) {
-      self.winston.log('info','cards file updated, reloading...');
-      self._loadKnownCards();
-    }   
-  });
+  console.log(this.cardsFile);
+  if(this.cardsFile){
+    this._loadKnownCards();
+    fs.watchFile(this.knownCardsFile, function(curr,prev) {
+      if (curr.mtime != prev.mtime) {
+        self.winston.log('info','cards file updated, reloading...');
+        self._loadKnownCards();
+      }   
+    });
+  }
 }
 
 CardReader.prototype._loadKnownCards = function (){
@@ -25,7 +30,7 @@ CardReader.prototype._loadKnownCards = function (){
   //TODO: explicitly catch invalid parse?
   this.knownCards = JSON.parse(data);
   //For some reason via the watchFile callback the above doesn't update us in memory... :s
-  this.winston.log('info', "loaded %s cards from file", (Object.keys(this.knownCards)).length);
+  this.winston.log('info', "loaded %s cards from file %s", (Object.keys(this.knownCards)).length, this.cardsFile);
 }
 
 CardReader.prototype.read = function(callback) {
@@ -85,5 +90,31 @@ CardReader.prototype.onFoundCard = function(callback) {
     }
   });
 };
+
+// set http refresh url and interval, Interval is in minutes
+CardReader.prototype.setHttpRefresher = function(httpurl, refreshinterval) {
+  var interval = refreshinterval * 60 * 1000;
+  var self = this;
+  this.winston.log('info','loading new file from %s', httpurl);
+  var options = { 'headers': {'Cookie':'SECRET=whateveryouwanthereitsnotimportant'},
+                  'url': httpurl};
+
+  //-b 'SECRET=whateveryouwanthereitsnotimportant'
+  http.get(options, function(res){
+    res.on('data', function(d) {
+      console.log(d);
+      var newCards = JSON.parse(d);
+      //TODO validate contents
+      this.knownCards = newCards;
+      this.winston.log('info', "loaded %s cards from web", (Object.keys(this.knownCards)).length);
+    });
+  }).on('error',function(e){
+    self.winston.log('error','error loading new file from %s',httpurl);
+    self.winston.log('error', e);
+  });
+  setTimeout(function() { 
+    self.setHttpRefresher(httpurl,refreshinterval);
+  }, interval);
+}
 
 module.exports = CardReader;
--- example.js
+++ example.js
@@ -2,7 +2,11 @@
 var winston = require('winston');
 var CardReader = require('./cardReader.js');
 
-var reader = new CardReader({'device':'test.data', 'knownCardsFile':'test.knownCards.json', "winston":winston});
+//var reader = new CardReader({'device':'test.data', 'knownCardsFile':'test.knownCards.json', "winston":winston});
+var reader = new CardReader({'device':'test.data', "winston":winston});
+
+//reader.setHttpRefresher('https://github.com/so-make-it/door-controller/raw/master/test.data2', 1);
+reader.setHttpRefresher('http://localhost:1337/adminapi/cards',1);
 reader.onFoundCard( function(card,state){
   console.log("''%s'' (%s)",card.username,state)
 });
